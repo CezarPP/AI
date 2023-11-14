4c0757b56ae684ca89ae160ef30be00c12535771
CezarPP <cezarpopoveniuc51@gmail.com>
2023-11-14 10:08:43 +0200

    Add sigmoid, relu and their derivatives

--- Homework4/ActivationFunction.py
+++ Homework4/ActivationFunction.py
@@ -1,1 +1,17 @@
-# will have derivative + normal -> this is for random layer
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_derivative(x):
+    return np.where(x > 0, 1, 0)
